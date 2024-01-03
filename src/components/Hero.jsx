332f70608150d803b25b8b6d1021a1ac0f7d59be
khushalGrover <khushal15Grover@gmail.com>
2024-01-03 23:46:19 +0530

    small changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,38 +2,38 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 export const Hero = ({ scrolled, scrollValue }) => {
 
-    // let result;
+    let result;
 
-    // if (scrollValue < 150) 
-    // {
-    //   result = 0;
-    // } 
-    // else if (scrollValue >= 150 && scrollValue < 250) 
-    // {
-    //   result = 1;
-    // } 
-    // else if (scrollValue >= 250 && scrollValue < 350) 
-    // {
-    //   result = 2;
-    // } 
-    // else if (scrollValue >= 350 && scrollValue < 450) 
-    // {
-    //   result = 3;
-    // } if (scrollValue >= 450 && scrollValue < 550) 
-    // {
-    //   result = 4;
-    // } if (scrollValue >= 550 && scrollValue < 650) 
-    // {
-    //   result = 5;
-    // }
+    if (scrollValue < 150) 
+    {
+      result = 0;
+    } 
+    else if (scrollValue >= 150 && scrollValue < 250) 
+    {
+      result = 1;
+    } 
+    else if (scrollValue >= 250 && scrollValue < 350) 
+    {
+      result = 2;
+    } 
+    else if (scrollValue >= 350 && scrollValue < 450) 
+    {
+      result = 3;
+    } if (scrollValue >= 450 && scrollValue < 550) 
+    {
+      result = 4;
+    } if (scrollValue >= 550 && scrollValue < 650) 
+    {
+      result = 5;
+    }
 
-    // const isEven = result % 2 === 0;
+    const isEven = result % 2 === 0;
 
     // let myClassName;
     // if (scrolled) 
     // {
     //   // myClassName = isEven ? "overlay left" : "overlay right";
-    //   console.log(scrollValue, "scrolled, result:", result, isEven ? "even" : "odd");
+    // //   console.log(scrollValue, "scrolled, result:", result, isEven ? "even" : "odd");
     // } 
     // else 
     // {
@@ -44,14 +44,24 @@ export const Hero = ({ scrolled, scrollValue }) => {
 
     return (
         <div className='overlay'>
-            <Canvas camera={ {position: [5,0,0]}}>
+            <Canvas camera={ 
+                {
+                    position: [3,2,0],
+                    rotation: [0,1  ,0]
+                
+                }}>
                 {/* disable zoom and scroll in orbitcontrol */}
-                <mesh position={[0, 0, 0]}>
+                <mesh position={[0, 0, -2]} scale= {isEven ?  [.01,.01 ,.01] : [1,1,1] }>
                     <boxGeometry />
                     
                     <meshNormalMaterial />
                 </mesh>
-                < OrbitControls enableZoom={false} enablePan={false} autoRotate={true} />
+                <mesh position={[0, 0, 2]} scale= {isEven ? [1,1,1] : [.01,.01 ,.01]}>
+                    <boxGeometry />
+                    
+                    <meshNormalMaterial />
+                </mesh>
+                < OrbitControls enableZoom={false} enablePan={false} autoRotate={false} />
             </Canvas>
         </div>
        
